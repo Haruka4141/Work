3e80f5717ed8597bb9621d9425618e7172e66aad
howard <howard.chang@aicipc.com.tw>
2022-02-07 14:58:42 +0800

    Add fru_fab_pn, YB_KSB_PN, ATHENA_MBPN compare

--- SN_Check.c
+++ SN_Check.c
@@ -10,8 +10,11 @@ typedef struct shopfloor shopfloor;
 struct shopfloor
 {
     char BLADE_SN[50];   //fru_serial_number (dm*)
-    char KSB_SN_1[50];   //fru_vendor_serial(ksb*/1)
-    char KSB_SN_2[50];   //fru_vendor_serial(ksb*/2)
+    char ATHENA_MBPN[50];//fru_fab_pn (dm*)
+    char KSB_SN_1[50];   //fru_vendor_serial (ksb*/1)
+    char KSB_SN_2[50];   //fru_vendor_serial (ksb*/2)
+    char YB_KSB_PN[50];  //fru_fab_pn (ksb*/1)
+    char YB_KSB_PN2[50]; //fru_fab_pn (ksb*/2)
 }shopfloor_array[10];
 
 typedef struct dm dm;
@@ -19,8 +22,11 @@ struct dm
 {
     char fru_serial_number[50];
     char fru_vendor_serial[50];
+    char dm_fru_fab_pn[50];
     char ksb1_fru_vendor_serial[50];
+    char ksb1_fru_fab_pn[50];
     char ksb2_fru_vendor_serial[50];
+    char ksb2_fru_fab_pn[50];
 }dm_array[10];
 //-------------------------------
 
@@ -60,6 +66,12 @@ int main()
             dm_array[index].fru_serial_number[str_length] = '\0';
             
         }
+        else if (dm_start && strstr(buffer, "fru_fab_pn: "))
+        {
+            str_length = strlen(buffer + 14) - 1;
+            strcpy(dm_array[index].dm_fru_fab_pn, buffer + 14);
+            dm_array[index].dm_fru_fab_pn[str_length] = '\0';
+        }
         else if (dm_start && strstr(buffer, "fru_vendor_serial: "))
         {
             str_length = strlen(buffer + 21) - 1;
@@ -67,12 +79,27 @@ int main()
             dm_array[index].fru_vendor_serial[str_length] = '\0';
             ++index;
         }
+
         // KSB-------------------------------------------------------
         else if (strstr(buffer, "========== FRU data of KSB =========="))
         {
             ksb_start = 1;
             dm_start = 0;
             index = 0;
+        }
+        else if (ksb_start && strstr(buffer, "fru_fab_pn: "))
+        {
+            str_length = strlen(buffer + 14) - 1;
+            if (ksb_switch)
+            {
+                strcpy(dm_array[index].ksb1_fru_fab_pn, buffer + 14);
+                dm_array[index].ksb1_fru_fab_pn[str_length] = '\0';
+            }
+            else
+            {
+                strcpy(dm_array[index].ksb2_fru_fab_pn, buffer + 14);
+                dm_array[index].ksb2_fru_fab_pn[str_length] = '\0';
+            }
         }
         else if (ksb_start && strstr(buffer, "fru_vendor_serial: "))
         {
@@ -141,7 +168,32 @@ int main()
                 strcpy(shopfloor_array[index].KSB_SN_1, buffer + 7);
                 shopfloor_array[index].KSB_SN_1[str_length] = '\0';
             }
+            else if (strstr(buffer, "ATHENA MBPN"))
+            {
+                str_length = strlen(buffer + 12) - 1;
+                memcpy(shopfloor_array[index].ATHENA_MBPN, buffer + 12, str_length - 3);
+                strcat(shopfloor_array[index].ATHENA_MBPN, " R");
+                strcat(shopfloor_array[index].ATHENA_MBPN, buffer + 23);
+                shopfloor_array[index].ATHENA_MBPN[str_length + 1] = '\0';
+            }
+            else if (strstr(buffer, "YB KSB PN2"))
+            {
+                str_length = strlen(buffer + 11) - 1;
+                memcpy(shopfloor_array[index].YB_KSB_PN2, buffer + 11, str_length - 3);
+                strcat(shopfloor_array[index].YB_KSB_PN2, " R");
+                strcat(shopfloor_array[index].YB_KSB_PN2, buffer + 22);
+                shopfloor_array[index].YB_KSB_PN2[str_length + 1] = '\0';
+            }
+            else if (strstr(buffer, "YB KSB PN"))
+            {
+                str_length = strlen(buffer + 10) - 1;
+                memcpy(shopfloor_array[index].YB_KSB_PN, buffer + 10, str_length - 3);
+                strcat(shopfloor_array[index].YB_KSB_PN, " R");
+                strcat(shopfloor_array[index].YB_KSB_PN, buffer + 21);
+                shopfloor_array[index].YB_KSB_PN[str_length + 1] = '\0';
+            }
         }
+
         fclose(shopfloor_log);
     }
     
@@ -158,23 +210,40 @@ int main()
         {
             printf("----------Difference!----------\n");
             printf("dm%-2d      fru_serial_number = %s\n", index+1, dm_array[index].fru_serial_number);
-            printf("shopfloor YB BLADE SN       = %s\n", shopfloor_array[index].BLADE_SN);
-
-        } 
+            printf("shopfloor YB BLADE SN       = %s\n\n", shopfloor_array[index].BLADE_SN);            
+        }
+        if (strcmp(dm_array[index].dm_fru_fab_pn, shopfloor_array[index].ATHENA_MBPN))
+        {
+            printf("----------Difference!----------\n");
+            printf("dm%-2d      fru_fab_pn        = %s\n", index+1, dm_array[index].dm_fru_fab_pn);
+            printf("shopfloor ATHENA MBPN       = %s\n\n", shopfloor_array[index].ATHENA_MBPN);
+        }
         if (strcmp(dm_array[index].ksb1_fru_vendor_serial, shopfloor_array[index].KSB_SN_1))
         {
             printf("----------Difference!----------\n");
-            printf("ksb%-2d/1    fru_vendor_serial = %s\n", index+1, dm_array[index].ksb1_fru_vendor_serial);
-            printf("shopfloor  KSB SN            = %s\n", shopfloor_array[index].KSB_SN_1);
+            printf("ksb%-2d/1   fru_vendor_serial = %s\n", index+1, dm_array[index].ksb1_fru_vendor_serial);
+            printf("shopfloor KSB SN            = %s\n\n", shopfloor_array[index].KSB_SN_1);
         }
         if (strcmp(dm_array[index].ksb2_fru_vendor_serial, shopfloor_array[index].KSB_SN_2))
         {
             printf("----------Difference!----------\n");
-            printf("ksb%-2d/2    fru_vendor_serial = %s\n", index+1, dm_array[index].ksb2_fru_vendor_serial);
-            printf("shopfloor  KSB SN2           = %s\n", shopfloor_array[index].KSB_SN_2);
+            printf("ksb%-2d/2   fru_vendor_serial = %s\n", index+1, dm_array[index].ksb2_fru_vendor_serial);
+            printf("shopfloor KSB SN2           = %s\n\n", shopfloor_array[index].KSB_SN_2);
+        }
+        if (strcmp(dm_array[index].ksb1_fru_fab_pn, shopfloor_array[index].YB_KSB_PN))
+        {
+            printf("----------Difference!----------\n");
+            printf("ksb%-2d/1   fru_fab_pn        = %s\n", index+1, dm_array[index].ksb1_fru_fab_pn);
+            printf("shopfloor YB KSB PN         = %s\n\n", shopfloor_array[index].YB_KSB_PN);
+        }
+        if (strcmp(dm_array[index].ksb2_fru_fab_pn, shopfloor_array[index].YB_KSB_PN2))
+        {
+            printf("----------Difference!----------\n");
+            printf("ksb%-2d/2   fru_fab_pn        = %s\n", index+1, dm_array[index].ksb2_fru_fab_pn);
+            printf("shopfloor YB KSB PN2        = %s\n\n", shopfloor_array[index].YB_KSB_PN2);
         }
     }
-
+    
     system("pause");
     return 0;
 }
